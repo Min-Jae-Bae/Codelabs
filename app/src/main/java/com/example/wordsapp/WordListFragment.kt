9234a081d76a1a8d3191cdfe42ecf6066c9674ea
Min-Jae-Bae <qoalswo123@icloud.com>
2022-06-07 00:53:50 +0900

    Hello

--- app/src/main/java/com/example/wordsapp/WordListFragment.kt
+++ app/src/main/java/com/example/wordsapp/WordListFragment.kt
@@ -7,7 +7,6 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.example.wordsapp.databinding.ActivityDetailBinding
 import com.example.wordsapp.databinding.FragmentWordListBinding
 
 
@@ -20,6 +19,8 @@ class WordListFragment : Fragment() {
 
     private var _binding: FragmentWordListBinding? = null
     private val binding get() = _binding!!
+    private lateinit var letterId: String   //lateinit은 null 허용하도록 만듬
+
 
 
     override fun onCreateView(
@@ -34,17 +35,20 @@ class WordListFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         val recyclerView = binding.recyclerView
         recyclerView.layoutManager = LinearLayoutManager(requireContext())
-        recyclerView.adapter = WordAdapter(activity?.intent?.extras?.getString(LETTER).toString(), requireContext())
+        recyclerView.adapter = WordAdapter(letterId, requireContext())
 
         recyclerView.addItemDecoration(
             DividerItemDecoration(context, DividerItemDecoration.VERTICAL)
         )
     }
 
+    // Bundle은 클래스 간에 데이터를 전달하는 데 사용되는 키-값 쌍으로 생각하세요
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val binding = ActivityDetailBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        // null이면 람다 실행되지 않음 -> let으로 행동
+        arguments?.let {
+            letterId = it.getString(LETTER).toString()
+        }
     }
 }
